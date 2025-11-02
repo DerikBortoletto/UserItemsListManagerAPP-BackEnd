6ebfc1676f22c8b2d13e9651156154fdd87b02d8
Derik Bortoletto <derikbortolettoti@gmail.com>
2025-11-02 16:25:36 -0300

    Added api-token-auth/ to urls and implements token auth upgrades

--- backend/urls.py
+++ backend/urls.py
@@ -4,6 +4,7 @@ from rest_framework import routers
 
 from .views import UserViewSet, GroupViewSet
 from core.views import ListViewSet, ItemViewSet
+from rest_framework.authtoken import views
 
 router = routers.DefaultRouter() 
 router.register(r"users", UserViewSet)
@@ -15,7 +16,8 @@ router.register(r"items", ItemViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include(router.urls)),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path('', include(router.urls)),
+    #path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path('api-token-auth/', views.obtain_auth_token, name='api-tokn-auth'),
 ]
 
