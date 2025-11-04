d05c3645bdf3082b66bfb105050f808375e61bc3
Derik Bortoletto <derikbortolettoti@gmail.com>
2025-11-03 21:24:55 -0300

    Configured ip into settings.py allowed hosts and core origin whitelist

--- backend/settings.py
+++ backend/settings.py
@@ -128,7 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "localhost",
+    "127.0.0.1",
     "3.21.162.220",
 ]
